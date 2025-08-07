28890934bfc1def3c0ecb6b648ff867e87c07a00
Apna Esport <apnaesportwork@gmail.com>
2025-08-07 12:36:48 +0000

    Make this Web site very fast because this takes many to

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,7 @@
 
+import { LoadingSpinner } from "@/components/shared/LoadingSpinner";
+
 export default function Loading() {
-  // This component is used by Next.js for navigation fallbacks.
-  // Returning null to remove the global loading UI.
-  return null;
+  // A global loading spinner that appears during page navigation.
+  return <LoadingSpinner fullPage showLogo showProgressBar />;
 }
