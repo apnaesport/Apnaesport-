d64c6fe397c80bbe0aa74ff16622c926e3cf40f1
Babu lal <babulalprj87@gmail.com>
2025-10-04 12:28:29 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Switched to clie

--- src/app/tournaments/[tournamentId]/page.tsx
+++ src/app/tournaments/[tournamentId]/page.tsx
@@ -46,7 +46,9 @@ export async function generateMetadata({ params }: TournamentPageProps, parent: 
   const startDate = toDate(tournament.startDate);
   const prizePool = (tournament.entryFee || 0) * (tournament.maxParticipants || 0);
   const title = `${tournament.name} | ${tournament.gameName} Tournament | Apna Esport`;
-  const description = `Join the ${tournament.name} ${tournament.gameName} tournament on Apna Esport. Starts on ${formatInTimeZone(startDate, 'Asia/Kolkata', "PPP")}. ${prizePool > 0 ? `Prize Pool: ${prizePool} 'AE Points'.` : ''} Sign up now!`;
+  const prizeText = prizePool > 0 ? `Prize Pool: ${prizePool} 'AE Points'.` : '';
+  const description = `Join the ${tournament.name} ${tournament.gameName} tournament on Apna Esport. Starts on ${formatInTimeZone(startDate, 'Asia/Kolkata', "PPP")}. ${prizeText} Sign up now!`;
+
 
   return {
     title,
@@ -87,4 +89,5 @@ export default function TournamentPage({ params }: TournamentPageProps) {
 }
 
     
+
 
